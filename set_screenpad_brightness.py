dc871b6c91472d636ee69a55376ca4d05feb0e95
elfakyn <elfakyn@gmail.com>
2021-04-07 11:24:01 -0400

    refactor

--- set_screenpad_brightness.py
+++ set_screenpad_brightness.py
@@ -3,12 +3,22 @@ import ctypes.wintypes
 import struct
 import sys
 
+KERNEL_32 = 'kernel32'
+
 INPUT_BUFFER_SIZE = 0x10
 OUTPUT_BUFFER_SIZE = 0x400
 DWORD_SIZE = 4
 
+COMMAND_STRUCTURE = '<IIII'
 COMMAND_ASUS_CONTROL = 0x53564544
+COMMAND_LENGTH = 8
+
 COMMAND_SET_BRIGHTNESS = 0x50032
+BRIGHTNESS_MIN = 0
+BRIGHTNESS_MAX = 255
+
+HANDLE_INVALID = ctypes.wintypes.DWORD(-1)
+HANDLE_NONE = ctypes.wintypes.DWORD(0)
 
 DEVICE_DRIVER = ctypes.wintypes.LPCWSTR('\\\\.\\ATKACPI')
 DEVICE_DESIRED_ACCESS = ctypes.wintypes.DWORD(0xc0000000)
@@ -21,14 +31,22 @@ DEVICE_HANDLE = None
 DEVICE_CONTROL_CODE = ctypes.wintypes.DWORD(0x22240c)
 DEVICE_LPOVERLAPPED = None
 
-ERROR_INVALID_BRIGHTNESS_VALUE = "The brightness must be a number between 0 (backlight off) and 255 (maximum brightness)."
-ERROR_MISSING_BRIGHTNESS_PARAMETER = "Please provide a brightness between 0 and 255. For example: python set_screenpad_brightness.py 200"
+ARGUMENT_POSITION_BRIGHTNESS = 1
+
+EXCEPTION_BRIGHTNESS_OOB = 'Brightness out of bounds'
+ERROR_INVALID_BRIGHTNESS_VALUE = 'The brightness must be a number between 0 (backlight off) and 255 (maximum brightness).'
+ERROR_MISSING_BRIGHTNESS_PARAMETER = 'Please provide a brightness between 0 and 255. For example: python set_screenpad_brightness.py 200'
+ERROR_BAD_HANDLE = 'Error! Cannot connect to the ASUS ScreenPad Driver.'
+
+EXIT_OK = 0
+EXIT_USAGE = 1
+EXIT_BAD_HANDLE = 2
 
 def change_brightness(brightness_value):
     execute_device_command(COMMAND_SET_BRIGHTNESS, brightness_value)
 
 def execute_device_command(command_id, command_parameter):
-    kernel32 = ctypes.WinDLL('kernel32')
+    kernel32 = ctypes.WinDLL(KERNEL_32)
 
     driver_handle = kernel32.CreateFileW(
         DEVICE_DRIVER,
@@ -40,47 +58,49 @@ def execute_device_command(command_id, command_parameter):
         DEVICE_HANDLE
     )
 
-    if driver_handle != ctypes.wintypes.DWORD(-1):
-        if driver_handle != ctypes.wintypes.DWORD(0):
-            command = struct.pack(
-                '<IIII',
-                COMMAND_ASUS_CONTROL,
-                8,
-                command_id,
-                command_parameter
-            )
+    if driver_handle == HANDLE_INVALID or driver_handle == HANDLE_NONE:
+        print(ERROR_BAD_HANDLE)
+        exit(EXIT_BAD_HANDLE)
+    
+    command = struct.pack(
+        COMMAND_STRUCTURE,
+        COMMAND_ASUS_CONTROL,
+        COMMAND_LENGTH,
+        command_id,
+        command_parameter
+    )
 
-            device_input_buffer = ctypes.create_string_buffer(command, INPUT_BUFFER_SIZE)
-            device_output_buffer = ctypes.create_string_buffer(OUTPUT_BUFFER_SIZE)
+    device_input_buffer = ctypes.create_string_buffer(command, INPUT_BUFFER_SIZE)
+    device_output_buffer = ctypes.create_string_buffer(OUTPUT_BUFFER_SIZE)
+    num_returned_bytes = ctypes.wintypes.LPDWORD()
 
-            num_returned_bytes = ctypes.wintypes.LPDWORD()
+    kernel32.DeviceIoControl(
+        driver_handle,
+        DEVICE_CONTROL_CODE,
+        device_input_buffer,
+        ctypes.wintypes.DWORD(INPUT_BUFFER_SIZE),
+        device_output_buffer,
+        ctypes.wintypes.DWORD(OUTPUT_BUFFER_SIZE),
+        num_returned_bytes,
+        DEVICE_LPOVERLAPPED
+    )
 
-            kernel32.DeviceIoControl(
-                driver_handle,
-                DEVICE_CONTROL_CODE,
-                device_input_buffer,
-                ctypes.wintypes.DWORD(INPUT_BUFFER_SIZE),
-                device_output_buffer,
-                ctypes.wintypes.DWORD(OUTPUT_BUFFER_SIZE),
-                num_returned_bytes,
-                DEVICE_LPOVERLAPPED
-            )
-
-        if driver_handle != ctypes.wintypes.DWORD(0xf):
-            kernel32.CloseHandle(driver_handle)
-            driver_handle = None
+    kernel32.CloseHandle(driver_handle)
+    driver_handle = None
 
 if __name__ == "__main__":
     try:
-        new_brightness = int(sys.argv[1])
-        if new_brightness < 0 or new_brightness > 255:
-            raise ValueError('Brightness out of bounds')
+        new_brightness = int(sys.argv[ARGUMENT_POSITION_BRIGHTNESS])
+        if new_brightness < BRIGHTNESS_MIN or new_brightness > BRIGHTNESS_MAX:
+            raise ValueError(EXCEPTION_BRIGHTNESS_OOB)
 
         change_brightness(new_brightness)
 
     except ValueError:
         print(ERROR_INVALID_BRIGHTNESS_VALUE)
-        exit(1)
+        exit(EXIT_USAGE)
     except IndexError:
         print(ERROR_MISSING_BRIGHTNESS_PARAMETER)
-        exit(1)
+        exit(EXIT_USAGE)
+        
+    exit(EXIT_OK)
